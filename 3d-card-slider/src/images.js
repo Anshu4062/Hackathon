21a4e6cf651e1784ce355c57536e5c5973175f7c
Suryansh Singh <supremesofficialyt@gmail.com>
2024-06-23 16:02:26 +0530

    Add files via upload

---
+++ 3d-card-slider/src/images.js
@@ -0,0 +1,113 @@
+import img1 from "./images/img1.jpg";
+import img2 from "./images/img2.jpg";
+import img3 from "./images/img3.jpg";
+import img4 from "./images/img4.jpg";
+import img5 from "./images/img5.jpg";
+import img6 from "./images/img6.jpg";
+import img7 from "./images/img7.jpg";
+import img8 from "./images/img8.jpg";
+import img9 from "./images/img9.jpg";
+import img10 from "./images/img10.jpg";
+import img11 from "./images/img11.jpg";
+import img12 from "./images/img12.jpg";
+import img13 from "./images/img13.jpg";
+import img14 from "./images/img14.jpg";
+import img15 from "./images/img15.jpg";
+import img16 from "./images/img16.jpg";
+import img17 from "./images/img17.jpg";
+import img18 from "./images/img18.jpg";
+import img19 from "./images/img19.jpg";
+import img20 from "./images/img20.jpg";
+import img21 from "./images/img21.jpg";
+import img22 from "./images/img22.jpg";
+import img23 from "./images/img23.jpg";
+import img24 from "./images/img24.jpg";
+import img25 from "./images/img25.jpg";
+import img26 from "./images/img26.jpg";
+// import img27 from "./images/img27.jpg";
+// import img28 from "./images/img28.jpg";
+// import img29 from "./images/img29.jpg";
+// import img30 from "./images/img30.jpg";
+// import img31 from "./images/img31.jpg";
+// import img32 from "./images/img32.jpg";
+// import img33 from "./images/img33.jpg";
+// import img34 from "./images/img34.jpg";
+// import img35 from "./images/img35.jpg";
+// import img36 from "./images/img36.jpg";
+// import img37 from "./images/img37.jpg";
+// import img38 from "./images/img38.jpg";
+// import img39 from "./images/img39.jpg";
+// import img40 from "./images/img40.jpg";
+// import img41 from "./images/img41.jpg";
+// import img42 from "./images/img42.jpg";
+// import img43 from "./images/img43.jpg";
+// import img44 from "./images/img44.jpg";
+// import img45 from "./images/img45.jpg";
+// import img46 from "./images/img46.jpg";
+// import img47 from "./images/img47.jpg";
+// import img48 from "./images/img48.jpg";
+// import img49 from "./images/img49.jpg";
+// import img50 from "./images/img50.jpg";
+// import img51 from "./images/img51.jpg";
+// import img52 from "./images/img52.jpg";
+// import img53 from "./images/img53.jpg";
+// import img54 from "./images/img54.jpg";
+// import img55 from "./images/img55.jpg";
+// import img56 from "./images/img56.jpg";
+// import img57 from "./images/img57.jpg";
+// import img58 from "./images/img58.jpg";
+// import img59 from "./images/img59.jpg";
+// import img60 from "./images/img60.jpg";
+// import img61 from "./images/img61.jpg";
+// import img62 from "./images/img62.jpg";
+// import img63 from "./images/img63.jpg";
+// import img64 from "./images/img64.jpg";
+// import img65 from "./images/img65.jpg";
+// import img66 from "./images/img66.jpg";
+// import img67 from "./images/img67.jpg";
+// import img68 from "./images/img68.jpg";
+// import img69 from "./images/img69.jpg";
+// import img70 from "./images/img70.jpg";
+// import img71 from "./images/img71.jpg";
+// import img72 from "./images/img72.jpg";
+// import img73 from "./images/img73.jpg";
+// import img74 from "./images/img74.jpg";
+// import img75 from "./images/img75.jpg";
+// import img76 from "./images/img76.jpg";
+// import img77 from "./images/img77.jpg";
+// import img78 from "./images/img78.jpg";
+// import img79 from "./images/img79.jpg";
+// import img80 from "./images/img80.jpg";
+// import img81 from "./images/img81.jpg";
+// import img82 from "./images/img82.jpg";
+// import img83 from "./images/img83.jpg";
+// import img84 from "./images/img84.jpg";
+// import img85 from "./images/img85.jpg";
+// import img86 from "./images/img86.jpg";
+// import img87 from "./images/img87.jpg";
+// import img88 from "./images/img88.jpg";
+// import img89 from "./images/img89.jpg";
+// import img90 from "./images/img90.jpg";
+// import img91 from "./images/img91.jpg";
+// import img92 from "./images/img92.jpg";
+// import img93 from "./images/img93.jpg";
+// import img94 from "./images/img94.jpg";
+// import img95 from "./images/img95.jpg";
+// import img96 from "./images/img96.jpg";
+// import img97 from "./images/img97.jpg";
+// import img98 from "./images/img98.jpg";
+// import img99 from "./images/img99.jpg";
+// import img100 from "./images/img100.jpg";
+
+
+const images = [
+    img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26, 
+    // img27, img28, img29, img30, img31, img32, img33, img34, img35, img36, img37, img38, img39, img40, img41,
+    // img42, img43, img44, img45, img46, img47, img48, img49, img50, img51, img52, img53, img54, img55, img56, img57, img58, img59,
+    // img60, img61, img62, img63, img64, img65, img66, img67, img68, img69, img70, img71, img72, img73, img74, img75, img76, img77,
+    // img78, img79, img80, img81, img82, img83, img84, img85, img86, img87, img88, img89, img90, img91, img92, img93, img94, img95,
+    // img96, img97, img98, img99, img100
+
+]
+
+export default images;
